b468c1d887e9e12e206ce44568b824ed34a81760
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2022-11-02 19:06:02 +0100

    noviembre

--- queries.js
+++ queries.js
@@ -18,7 +18,7 @@ const pool = new Pool({
   user: "postgres",
   host: "containers-us-west-72.railway.app",
   database: "railway",
-  password: "jR4YUasS06lS9YS0Ywq6",
+  password: "Hr7PSUpLGjxglo9ob9e7",
   port: 6186,
   ssl:{
     rejectUnauthorized:false
